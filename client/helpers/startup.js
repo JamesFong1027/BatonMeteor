644e657f15349c42fdf82960f07fc37de6061988
Victor Chen <Victor.Chen@pointclickcare.com>
2016-10-30 15:30:44 -0400

    add initial i18n package and initial setup

--- client/helpers/startup.js
+++ client/helpers/startup.js
@@ -145,4 +145,24 @@ Meteor.startup(function() {
 	Math.radians = function(degrees) {
 		return degrees * Math.PI / 180;
 	}
+
+
+	// language setup
+	getUserLanguage = function () {
+	  // Put here the logic for determining the user language
+	  return "en";
+	};
+
+	if (Meteor.isClient) {
+		Session.set("showLoadingIndicator", true);
+
+		TAPi18n.setLanguage(getUserLanguage())
+			.done(function() {
+				Session.set("showLoadingIndicator", false);
+			})
+			.fail(function(error_message) {
+				// Handle the situation
+				console.log(error_message);
+			});
+	}
 });
